ffcfb6cdffd0bf301417abf63b7c7bb2824cccf5
noodlez04 <noodlez040@gmail.com>
2017-08-26 23:05:40 +0300

    Update gitignore file. Update installing lua + torch script.

--- utils/install_lua_deps.sh
+++ utils/install_lua_deps.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+# Install required dependencies
+sudo apt-get install libboost-all-dev libpython2.7 openjdk-8-jdk lua5.1 libxerces-c3.1 liblua5.1-0-dev ffmpeg python-tk python-imaging-tk
+sudo update-ca-certificates -f
+
+# Install torch
+git clone https://github.com/torch/distro.git ~/torch --recursive
+cd ~/torch; bash install-deps;
+TORCH_LUA_VERSION=LUA51 ./install.sh
+source ~/.bashrc
+
+
+# Install all required lua rocks
 rocks=(luasocket logroll moses classic cwrap paths nn cutorch cunn luafilesystem penlight sys xlua image env qtlua qttorch nngraph luaposix nninit rnn tds socket.core underscore)
 
 for rock in ${rocks[*]}; do
